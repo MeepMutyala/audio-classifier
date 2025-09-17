424e803dd50ccb0c5707516fe8453d3f2aa2514a
Apuroop Mutyala <apumutyala@gmail.com>
2025-09-17 13:13:42 -0400

    reverting src addition to path in init.py

--- external_models/__init__.py
+++ external_models/__init__.py
@@ -3,13 +3,10 @@ import os
 
 # Add both submodule src directories to Python path
 _current_dir = os.path.dirname(__file__)
+_liquid_s4_src = os.path.join(_current_dir, 'liquid-S4')
+_vjepa2_src = os.path.join(_current_dir, 'vjepa2')
 
-# Add the src directories, not the root directories
-_liquid_s4_src = os.path.join(_current_dir, 'liquid-S4', 'src')
-_vjepa2_src = os.path.join(_current_dir, 'vjepa2', 'src') 
-_mamba_src = os.path.join(_current_dir, 'mamba', 'src')
-
-# Add to path if not already there  
-for path in [_liquid_s4_src, _vjepa2_src, _mamba_src]:
-    if os.path.exists(path) and path not in sys.path:
+# Add to path if not already there
+for path in [_liquid_s4_src, _vjepa2_src]:
+    if path not in sys.path:
         sys.path.insert(0, path)
